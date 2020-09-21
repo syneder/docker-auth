951c01367c9aa9e513c00eea54d65771d704a9c1
Denys <67856698+syneder@users.noreply.github.com>
2020-09-21 08:50:02 +0300

    Added a runtime authentication context that describes authentication-specific information about an individual request

---
+++ src/Docker.Registry.Authentication.Configuration/Runtime/RuntimeAuthenticationContext.cs
@@ -0,0 +1,26 @@
+﻿using Docker.Registry.Authentication.Configuration.Runtime.Models;
+using System.Collections.Generic;
+
+namespace Docker.Registry.Authentication.Configuration.Runtime
+{
+    /// <summary>
+    /// Encapsulates all Docker Registry authentication specific information about an individual request.
+    /// </summary>
+    public class RuntimeAuthenticationContext
+    {
+        /// <summary>
+        /// Gets or sets specific information about an authenticated account.
+        /// </summary>
+        public Account AuthenticatedAccount { get; set; }
+
+        /// <summary>
+        /// Gets the <see cref="AuthenticationTokenOptions"/> for the authentication system.
+        /// </summary>
+        public AuthenticationTokenOptions Options { get; internal set; }
+
+        /// <summary>
+        /// Gets or sets an enumeration of permissions for an authenticated user or individual request.
+        /// </summary>
+        public IEnumerable<RepositoryPermission> Permissions { get; set; }
+    }
+}
---
+++ src/Docker.Registry.Authentication.Configuration/Runtime/RuntimeAuthenticationContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+
+namespace Docker.Registry.Authentication.Configuration.Runtime
+{
+    /// <summary>
+    /// A factory that can create <see cref="RuntimeAuthenticationContext"/> instances.
+    /// </summary>
+    public class RuntimeAuthenticationContextFactory
+    {
+        private readonly AuthenticationTokenOptions _options;
+
+        /// <summary>
+        /// Creates a new <see cref="RuntimeAuthenticationContext"/>.
+        /// </summary>
+        /// <param name="options">The <see cref="AuthenticationTokenOptions"/>.</param>
+        public RuntimeAuthenticationContextFactory(IOptions<AuthenticationTokenOptions> options)
+        {
+            _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        /// <summary>
+        /// Creates an <see cref="RuntimeAuthenticationContext"/>.
+        /// </summary>
+        /// <returns>The <see cref="RuntimeAuthenticationContext"/>.</returns>
+        public RuntimeAuthenticationContext Create() => new RuntimeAuthenticationContext
+        {
+            Options = _options
+        };
+    }
+}
